7ff69d18245dd71ef13a06d73b96004a808a7b7b
AditiDhumal2 <167055939+AditiDhumal2@users.noreply.github.com>
2025-07-17 18:20:21 +0530

    Connect frontend to deployed backend

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from backend.chatbot import get_bot_response
+from backend.chatbot import get_response
 
 app = Flask(__name__)
 CORS(app)  # Enable Cross-Origin requests (for frontend connection)
@@ -9,7 +9,7 @@ CORS(app)  # Enable Cross-Origin requests (for frontend connection)
 def chat():
     data = request.get_json()
     user_input = data.get("message")
-    reply = get_bot_response(user_input)
+    reply = get_response(user_input)
     return jsonify({"reply": reply})
 
 if __name__ == "__main__":
